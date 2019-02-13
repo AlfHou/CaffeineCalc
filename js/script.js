afd6017f8e7668379781345e8bb7165e19f92fb8
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2019-02-13 14:32:55 +0100

    Add javascript and some more content

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var submitButton = document.getElementById("enter");
+
+	submitButton.addEventListener("click", function() {
+		//Calculate caffein in the number of cups the user supplied
+		let numberOfCups = document.getElementById("cupsdrunk").value;
+		displayResult(numberOfCups);
+	});
+	function displayResult(cups) {
+		var outputField = document.getElementById("output");
+
+		if (!isNaN(cups) && cups !== "") {
+			let totalCaffeine = cups * 94.8;
+			outputField.innerHTML = "In " + cups + " cups of coffee there is approximately: " + totalCaffeine + "mg of caffeine";
+
+		if (totalCaffeine >= 400) {
+			document.getElementById("warn").style.color = "red";	
+		}
+		} else {
+			outputField.innerHTML = "Please enter a number in the text box";
+		}
+	}
+});
